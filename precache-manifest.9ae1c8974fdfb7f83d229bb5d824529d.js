a9444caf371e6b845594e6ae67e86194a2c1451d
adayofmercury <adayofmercury@kakao.com>
2020-09-04 22:31:42 +0900

    Updates

--- precache-manifest.9ae1c8974fdfb7f83d229bb5d824529d.js
+++ precache-manifest.9ae1c8974fdfb7f83d229bb5d824529d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0d3467be7b2942e09fa52320c55b0038",
+    "revision": "d381db018833a21553d38c2a6b3c5984",
     "url": "/movie_app_2020/index.html"
   },
   {
-    "revision": "a6481ffe117c3161675f",
-    "url": "/movie_app_2020/static/css/main.f853f480.chunk.css"
+    "revision": "358fb1b459588f1150dc",
+    "url": "/movie_app_2020/static/css/main.89e1ce2c.chunk.css"
   },
   {
     "revision": "dcbc1ad3746847d335f9",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie_app_2020/static/js/2.968ebb96.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a6481ffe117c3161675f",
+    "revision": "358fb1b459588f1150dc",
     "url": "/movie_app_2020/static/js/main.e31a63ed.chunk.js"
   },
   {
